18c9b2759592a72c6de513f3c24ffe309386b195
sanjeevKumar <sanjeevkumarjv@gmail.com>
2020-04-13 16:13:43 +0530

    fourth-commit : setting up view engine and creating views

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
 module.exports.profile = function(req, res){
-    return res.end('<h1>Users profile</h1>')
+    // return res.end('<h1>Users profile</h1>')
+    return res.render('home',{
+        title: 'users-profile'
+    })
 }
