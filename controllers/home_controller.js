a9adf192a659d60a0404ee46fa7cbd2eba4c02f3
sanjeevKumar <sanjeevkumarjv@gmail.com>
2020-04-13 15:32:35 +0530

    second-commit : creating routers and controllers

---
+++ controllers/home_controller.js
@@ -0,0 +1,7 @@
+module.exports.home = function(req, res){
+    return res.end('<h1>Express is up for Codeial!</h1>')
+}
+
+// module.exports.play = function(req, res){
+//     return res.end('<h1>This is playGround for express</h1>')
+// }
--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.listen(function(err){
+// use express router
+app.use('/', require('./routes'));
+
+app.listen(port, function(err){
     if(err){
         console.log(`Error on running the server : ${err}`);
     }
     console.log(`Server is succesfully running on the port : ${port}`);
-})
+});
