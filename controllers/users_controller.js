2707e43ff74ea4c808f866693f9338102c57803a
sanjeevKumar <sanjeevkumarjv@gmail.com>
2020-04-17 10:51:59 +0530

    create a user schema and render pages for signUp and signIn

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,3 +4,28 @@ module.exports.profile = function(req, res){
         title: 'users-profile'
     })
 }
+
+// render sign up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title: 'Codeial | sign-up'
+    })
+}
+
+// render sign in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in',{
+        title: 'Codeial | sign-in'
+    })
+}
+
+// get the sign up data
+module.exports.create = function(req, res){
+    // todo later
+}
+
+// sign in  and craete a session for user
+module.exports.createSession = function(req, res){
+    // todo later
+}
+
