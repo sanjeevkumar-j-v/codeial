527252185abe38c1e454e29b12d22f77d6747033
sanjeevKumar <sanjeevkumarjv@gmail.com>
2020-04-19 00:52:52 +0530

    create sign-out and header

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -56,3 +56,7 @@ module.exports.createSession = function(req, res){
     return res.redirect('/');
 }
 
+module.exports.destroySession = function(req, res){
+    req.logout();
+    return res.redirect('/');
+}
