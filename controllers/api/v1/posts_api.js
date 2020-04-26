6f2725583af76b8571f1bf0ff494609abab232bd
sanjeevKumar <sanjeevkumarjv@gmail.com>
2020-04-26 11:25:59 +0530

    Authorizate user on API for deleting the post

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -28,7 +28,7 @@ module.exports.destroy = async function(req, res){
         let post = await Post.findById(req.params.id);
         // .id means converting the object id into string
         
-        // if(post.user == req.user.id){
+        if(post.user == req.user.id){
             post.remove();
 
             await Comment.deleteMany({post: req.params.id});
@@ -39,15 +39,17 @@ module.exports.destroy = async function(req, res){
                 message: "Post associated comments deleted successfully"
             });
 
-        // }else{
-        //     req.flash('error', 'Idiot, You cannot delete this post')
-        //     return res.redirect('back');
-        // }
+        }else{
+            // req.flash('error', 'Idiot, You cannot delete this post')
+            return res.json(401, {
+                message: "You cannot delete this post"
+            });
+        }
     }catch(err){
         console.log('Error ', err);
         return res.json(500, {
             message: "Internal server error"
-        })
+        });
     }
 
 }
